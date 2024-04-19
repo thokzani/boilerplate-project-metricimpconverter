1dffded4c34c4b5bd3382fbe8f3173a9e904ce3c
thokozani Maluleke <thokozanihlope2@gmail.com>
2024-04-19 12:15:25 +0200

    boilerplate solutions

--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,34 @@
-"use strict";
+'use strict';
 
-const expect = require("chai").expect;
-const ConvertHandler = require("../controllers/convertHandler.js");
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
 
-  app.route("/api/convert").get((req, res) => {
-    // console.log({ input: req.query.input });
-    if (req.query.input === undefined || req.query.input === "") {
-      res.status(404).type("text").send("invalid unit");
-    } else {
-      let str = String(req.query.input);
-      let initUnit, initNum;
-      let errors = [];
-      try {
-        initUnit = convertHandler.getUnit(str);
-      } catch (err) {
-        errors.push(err.message);
-      }
+  app.get('/api/convert', async (req, res) => {
+    const input = req.query.input;
 
-      try {
-        initNum = convertHandler.getNum(str);
-        if (errors.length > 0) return res.send(errors[0]);
-      } catch (err) {
-        if (errors.length === 0) return res.send(err.message);
-        return res.send(`${err.message} and unit`);
-      }
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      res.json({
-        initNum,
-        initUnit,
-        returnNum,
-        returnUnit,
-        string,
-      });
-    }
+    const response = {
+      'initNum': initNum,
+      'initUnit': initUnit,
+      'returnNum': returnNum,
+      'returnUnit': returnUnit,
+      'string': string
+    };
+
+    const invalidNumber = (initNum == convertHandler.invalid_number);
+    const invalidUnit = (initUnit == convertHandler.invalid_unit);
+
+    if (invalidNumber && invalidUnit) res.send(convertHandler.invalid_number_and_unit);
+    else if (invalidNumber) res.send(convertHandler.invalid_number);
+    else if (invalidUnit) res.send(convertHandler.invalid_unit);
+    else res.json(response);
   });
 };
